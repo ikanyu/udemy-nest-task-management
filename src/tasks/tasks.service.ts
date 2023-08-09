ea4de4a95a8d7f4e7bcb63306054f22386f64524
Wen Yu <wongwy93@gmail.com>
2023-08-09 21:18:14 +0800

    add validation pipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -28,16 +28,22 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    const task = this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
 
-    return task;
+    if (!found) {
+      throw new NotFoundException(`Task with "${id}" not found`);
+    }
+
+    return found;
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
-  updateTask(id: string, status: TaskStatus): Task {
+  updateTaskStatus(id: string, status: TaskStatus): Task {
     const task = this.getTaskById(id);
     task.status = status;
 
